400a0c59695b756b3215bf720f023906257dfad7
민석 <108412831+ka-MS@users.noreply.github.com>
2024-01-29 21:04:32 +0900

    feat: Add unit StaticFactoryTest

---
+++ src/test/java/com/example/bootproject/domain/StaticFactoryTestTest.java
@@ -0,0 +1,13 @@
+package com.example.bootproject.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class StaticFactoryTestTest {
+
+    @Test
+    void staticFactoryTest() {
+        StaticFactoryTest test = StaticFactoryTest.staticFactoryTest("", 1, "");
+    }
+}
